1b14d50861c3b635b1c54f41715b1f8dac6ef299
Estêvão Abreu <estevao.abreu40@gmail.com>
2025-12-16 17:13:26 +0000

    CSS Adjustments

--- src/frontend/components/FilterTab.jsx
+++ src/frontend/components/FilterTab.jsx
@@ -12,19 +12,21 @@ function FilterTab({ tags }) {
                     <span className="material-icons">filter_alt</span>
                     <span>Filters</span>
                 </label>
-                <select id="tags" data-placeholder="Tags">
+                <select id="tags">
                     {tags.map((tag, index) => (
                         <option key={index} value={tag}>{tag}</option>
                     ))}
                 </select>
                 <div className="cookingTime">
-                    <label>Cooking Time:</label>
+                    <label>Cooking Time: </label>
                     <input type="number" min="0" placeholder="Min"></input>
+                    -
                     <input type="number" max="0" placeholder="Max"></input>
                 </div>
                 <div className="servings">
-                    <label>Servings:</label>
+                    <label>Servings: </label>
                     <input type="number" min="0" placeholder="Min"></input>
+                    -
                     <input type="number" max="0" placeholder="Max"></input>
                 </div>
                 <button className="applyButton">
