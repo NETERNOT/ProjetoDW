ff7c7619a20bedd43f3f56edfdc21be11c4d30d3
NunoPinto <167584289+NETERNOT@users.noreply.github.com>
2025-12-18 15:51:30 +0000

    Custom Input added

---
+++ src/frontend/components/customInput.js
@@ -0,0 +1,26 @@
+const { useState } = React;
+
+function CustomInput(props){
+    const {label, type, value, onChange, errorMessage, isValid} = props;
+    const [touched, setTouched] = useState(false)
+
+    return(
+        <div className={`inputContainer ${value.length>0  ? "filled" : ""} ${!isValid && touched ? "invalid" : ""}`}>
+            <label htmlFor={type} >
+              <span>{label}</span>
+              <input
+                type={type}
+                value={value}
+                onChange={(e)=>{
+                    onChange(e);
+                    setTouched(false)
+                }}
+                onBlur={()=>setTouched(true)}
+                required
+              ></input>
+            </label>
+            <p className="errorMessage">{errorMessage}</p>
+          </div>
+    );
+}
+
