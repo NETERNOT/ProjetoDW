49757917d120d17251d0cb5fd8d0f292e46a5bdb
Estêvão Abreu <estevao.abreu40@gmail.com>
2025-12-20 14:47:13 +0000

    Fixed home and recipe views

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,12 +1,13 @@
 const { useState, useEffect } = React;
 
 function App() {
-    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
   const [recipes, setRecipes] = useState([]);
+  const [recipeId, setRecipeId] = useState(null);
   const [loading, setLoading] = useState(true);
   const [view, setView] = useState("login");
 
-    const [loginImage, setLoginImage] = useState(null);
+  const [loginImage, setLoginImage] = useState(null);
 
   const [userId, setUserId] = useState(null)
 
@@ -20,27 +21,31 @@ function App() {
         setRecipes(data);
         setLoading(false);
 
-                if (data.length > 0) {
-                    setLoginImage(data[Math.floor(Math.random() * data.length)]?.picture);
-                }
-            } catch (error) {
-                console.error("Error fetching recipes:", error);
-                setLoading(false);
-            }
-        };
+        if (data.length > 0) {
+          setLoginImage(data[Math.floor(Math.random() * data.length)]?.picture);
+        }
+      } catch (error) {
+        console.error("Error fetching recipes:", error);
+        setLoading(false);
+      }
+    };
 
     fetchRecipes();
   }, [recipes.length]);
 
- /*  const handleSeedData = async () => {
-    await fetch("http://localhost:8000/api/seed");
-    fetchRecipes(); // Refresh list after seeding
-  }; */
+  /*  const handleSeedData = async () => {
+     await fetch("http://localhost:8000/api/seed");
+     fetchRecipes(); // Refresh list after seeding
+   }; */
 
-  if(view ==="home") {console.log(userId)}
+  if (view === "home") { console.log(userId) }
   return (
     <div className="container">
-      {view === "home" && <div>Welcome Home!</div>}
+      {view === "home" &&
+        <HomeView recipes={recipes} onSelect={(id) => { setRecipeId(id); setView("recipe") }} onSelectNew={() => document.querySelector(".newRecipeContainer").classList.remove("hidden")} onCloseNew={() => document.querySelector(".newRecipeContainer").classList.add("hidden")} />}
+
+      {view === "recipe" &&
+        <RecipeView recipe={recipes.find((recipe) => recipe._id === recipeId)} onBack={() => setView("home")} />}
 
       {(view === "login" || view == "signup") && (
         <LoginView
